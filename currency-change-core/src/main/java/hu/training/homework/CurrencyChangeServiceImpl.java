8004aac8e804bec1d83b6e5600e05cea7bacf050
Stefán János <it.stefan.janos@gmail.com>
2019-01-27 02:29:20 +0100

    Create CurrencyChangeServiceImpl class

---
+++ currency-change-core/src/main/java/hu/training/homework/CurrencyChangeServiceImpl.java
@@ -0,0 +1,17 @@
+package hu.training.homework;
+
+public class CurrencyChangeServiceImpl implements CurrencyChangeService {
+
+	@Override
+	public double buy(double amount, Currency expectedCurreny) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double sell(double amount, Currency currentCurreny) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
