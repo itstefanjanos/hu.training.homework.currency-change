fb8caa523c6f3b99fffaf9814d3c726fa0cfb635
Stefán János <it.stefan.janos@gmail.com>
2019-01-27 02:47:46 +0100

    Implement buy and sell method in CurrencyChangeServiceImpl class

--- currency-change-core/src/main/java/hu/training/homework/CurrencyChangeServiceImpl.java
+++ currency-change-core/src/main/java/hu/training/homework/CurrencyChangeServiceImpl.java
@@ -3,15 +3,19 @@ package hu.training.homework;
 public class CurrencyChangeServiceImpl implements CurrencyChangeService {
 
 	@Override
-	public double buy(double amount, Currency expectedCurreny) {
-		// TODO Auto-generated method stub
-		return 0;
+	public double buy(double amount, Currency expectedCurreny) throws IllegalArgumentException {
+		if (amount <= 0.0) {
+			throw new IllegalArgumentException("The amount must be a positive number!");
+		}
+		return amount / CurrencyRatesMock.getCurrencyBuyingRate(expectedCurreny);
 	}
 
 	@Override
-	public double sell(double amount, Currency currentCurreny) {
-		// TODO Auto-generated method stub
-		return 0;
+	public double sell(double amount, Currency currentCurreny) throws IllegalArgumentException {
+		if (amount <= 0.0) {
+			throw new IllegalArgumentException("The amount must be a positive number!");
+		}
+		return CurrencyRatesMock.getCurrencyBuyingRate(currentCurreny) / amount;
 	}
 
 }
